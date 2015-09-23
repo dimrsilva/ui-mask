af8b75ea8ef285e70650ae9fe46388e85b8390e7
Anderson Ribeiro e Silva <anderonr@ciandt.com>
2015-09-23 14:54:13 -0300

    Add uiMaskService

---
+++ testv2/uiMaskServiceSpec.js
@@ -0,0 +1,59 @@
+describe("uiMaskService", function() {
+    "use strict";
+
+    var uiMask;
+
+    beforeEach(module("ui.mask.service"));
+
+    beforeEach(inject(function (_uiMask_) {
+        uiMask = _uiMask_;
+    }));
+
+    describe("initialization", function() {
+        it("should initialize", function() {
+            var mask = uiMask.create("AAA-9999");
+            expect(mask).not.toBeNull();
+        });
+    });
+
+    describe("mask", function() {
+        it("should mask", function() {
+            var mask = uiMask.create("AAA-9999");
+            var masked = mask.mask("abc1234");
+            expect(masked).toBe("abc-1234");
+        });
+
+        it("should mask partial values", function() {
+            var mask = uiMask.create("AAA-9999");
+            var masked = mask.mask("abc12");
+            expect(masked).toBe("abc-12__");
+        });
+
+        it("should not mask invalid values", function() {
+            var mask = uiMask.create("AAA-9999");
+            var masked = mask.mask("ab1234");
+            expect(masked).toBeNull();
+        });
+    });
+
+    describe("unmask", function() {
+        it("should unmask", function() {
+            var mask = uiMask.create("AAA-9999");
+            var unmasked = mask.unmask("abc-1234");
+            expect(unmasked).toBe("abc1234");
+        });
+
+        it("should unmask partial values", function() {
+            var mask = uiMask.create("AAA-9999");
+            var unmasked = mask.unmask("abc-12__");
+            expect(unmasked).toBe("abc12");
+        });
+
+        it("should not mask invalid values", function() {
+            var mask = uiMask.create("AAA-9999");
+            var unmasked = mask.unmask("ab1234");
+            expect(unmasked).toBeNull();
+        });
+    })
+
+});
---
+++ v2/uiMaskService.js
@@ -0,0 +1,146 @@
+angular.module('ui.mask.service', []).factory('uiMask', uiMaskFactory)
+        .value('uiMaskConfig', {
+            maskDefinitions: {
+                '9': /\d/,
+                'A': /[a-zA-Z]/,
+                '*': /[a-zA-Z0-9]/
+            },
+            clearOnBlur: true,
+            eventsToHandle: ['input', 'keyup', 'click', 'focus']
+        });
+
+function uiMaskFactory(uiMaskConfig) {
+	return {
+		create: function(maskDefinition, maskPlaceholder, maskOptions) {
+			var maskCaretMap,
+				maskPatterns;
+
+			initMask();
+
+			return {
+				mask: mask,
+				unmask: unmask,
+				isValid: isValid,
+			};
+
+			function initMask() {
+				if(typeof maskDefinition === "String") {
+					throw "Invalid maskDefinition";
+				}
+				maskOptions = uiMaskConfig;
+				processRawMask();
+			}
+
+			function mask(rawValue) {
+                var valueMasked = '',
+                    rvi = 0, //rawValueIndex
+                    mki = 0; // maskCaretMapIndex
+
+                for (var i = 0; i < maskPlaceholder.length; i++) {
+                    if (rvi < rawValue.length && i === maskCaretMap[mki]) {
+                        if (maskPatterns[mki].test(rawValue.charAt(rvi))) {
+                            valueMasked += rawValue.charAt(rvi) || '_';
+                            rawValue = rawValue.substr(1);
+                            mki++;
+                        }
+                        else {
+                            return null;
+                        }
+                    }
+                    else {
+                        valueMasked += maskPlaceholder.charAt(i);
+                    }
+                }
+                return valueMasked;
+			}
+
+			function unmask(maskedValue) {
+                var valueUnmasked = '',
+                    mpi = 0; // maskPatternsIndex
+
+                // Preprocess by stripping mask components from value
+                for (var i = 0; i < maskComponents.length; i++) {
+                    maskedValue = maskedValue.replace(maskComponents[i], '');
+                }
+
+                for (var i = 0; i < maskedValue.length; i++) {
+                    var chr = maskedValue[i];
+                    if (mpi <= maskPatterns.length && maskPatterns[mpi].test(chr)) {
+                        valueUnmasked += chr;
+                        mpi++;
+                    }
+                    else if (chr !== maskPlaceholder[i]) {
+                        return null;
+                    }
+                }
+                return valueUnmasked;
+			}
+
+			function isValid(rawValue) {
+
+			}
+
+            function processRawMask() {
+                var characterCount = 0;
+
+                maskCaretMap = [];
+                maskPatterns = [];
+                var defaultMaskPlaceholder = '';
+
+                if (typeof maskDefinition === 'string') {
+
+                    var isOptional = false,
+                            numberOfOptionalCharacters = 0,
+                            splitMask = maskDefinition.split('');
+
+                    angular.forEach(splitMask, function(chr, i) {
+                        if (maskOptions.maskDefinitions[chr]) {
+
+                            maskCaretMap.push(characterCount);
+
+                            defaultMaskPlaceholder += getPlaceholderChar(i - numberOfOptionalCharacters);
+                            maskPatterns.push(maskOptions.maskDefinitions[chr]);
+
+                            characterCount++;
+                        }
+                        else if (chr === '?') {
+                            isOptional = true;
+                            numberOfOptionalCharacters++;
+                        }
+                        else {
+                            defaultMaskPlaceholder += chr;
+                            characterCount++;
+                        }
+                    });
+                }
+                // Caret position immediately following last position is valid.
+                maskCaretMap.push(maskCaretMap.slice().pop() + 1);
+
+                maskPlaceholder = defaultMaskPlaceholder;
+
+                maskComponents = getMaskComponents();
+            }
+
+            function getPlaceholderChar(i) {
+                if (angular.isDefined(maskPlaceholder)) {
+                    return maskPlaceholder[i];
+                }
+                else {
+                	return '_';
+                }
+            }
+
+            // Generate array of mask components that will be stripped from a masked value
+            // before processing to prevent mask components from being added to the unmasked value.
+            // E.g., a mask pattern of '+7 9999' won't have the 7 bleed into the unmasked value.
+            // If a maskable char is followed by a mask char and has a mask
+            // char behind it, we'll split it into it's own component so if
+            // a user is aggressively deleting in the input and a char ahead
+            // of the maskable char gets deleted, we'll still be able to strip
+            // it in the unmaskValue() preprocessing.
+            function getMaskComponents() {
+                return maskPlaceholder.replace(/[_]+/g, '_').replace(/([^_]+)([a-zA-Z0-9])([^_])/g, '$1$2_$3').split('_');
+            }
+        }
+    };
+}
